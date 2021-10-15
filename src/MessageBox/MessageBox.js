66bbf530b7dc1d348acedc8117c53b2b23e8e016
ubuntu <anandapadmanabhanvt@gmail.com>
2021-10-15 23:24:49 +0530

    bottom

--- src/MessageBox/MessageBox.js
+++ src/MessageBox/MessageBox.js
@@ -145,7 +145,7 @@ export class MessageBox extends React.PureComponent {
                                             this.props.title &&
                                             <span>{this.props.title}</span>
                                         }
-                                      {!this.props.below?<div style={{bottom:'-27px'}}
+                                      {!this.props.below?<div style={{bottom:'-22px'}}
                                     className={classNames(
                                         'rce-mbox-time',
                                         { 'rce-mbox-time-block': thatAbsoluteTime },
@@ -302,7 +302,7 @@ export class MessageBox extends React.PureComponent {
                                 }
 
                                 {this.props.below?<div
-                                style={{bottom:'-27px'}}
+                                style={{bottom:'-22px'}}
                                     className={classNames(
                                         'rce-mbox-time',
                                         { 'rce-mbox-time-block': thatAbsoluteTime },
